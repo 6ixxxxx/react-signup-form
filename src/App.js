82069c0ca08f6f30f8403cd53e06f731d1937c23
6ixty9 <72061948+6ixty9@users.noreply.github.com>
2022-08-07 09:03:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    
+    
+    const [holdValues, setValues] = React.useState({
+        email:"", password:"", confirmPass:"", okayToEmail: false
+    })
+    function switchUp(event) {
+    const {name, value, type, checked} = event.target
+        setValues(prevValues =>{
+        return{...prevValues, 
+        [name]: type === "checkbox" ? checked : value}    
+        }
+      )
+    }
+    
+    function handleSubmit(event) {
+    const {password, confirmPass, okayToEmail} = holdValues
+        event.preventDefault()
+        let gg = "Input password"
+        password && confirmPass ? 
+        (password === confirmPass ? gg = "Successfully signed up" : gg = "passwords do not match") : gg = "Input password"
+        console.log(gg)
+        
+        (okayToEmail ? console.log("Thanks for signing up for our newsletter!"): "Not P")
+    }
+    
+    return (
+        <div className="form-container">
+            <form className="form" onSubmit={handleSubmit}>
+                <input 
+                    type="email" 
+                    placeholder="Email address"
+                    onChange={switchUp}
+                    name="email"
+                    className="form--input"
+                    value={holdValues.email}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password"
+                    onChange={switchUp}
+                    name="password"
+                    className="form--input"
+                    value={holdValues.password}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Confirm password"
+                    onChange={switchUp}
+                    name="confirmPass"
+                    className="form--input"
+                    value={holdValues.confirmPass}
+                />
+                
+                <div className="form--marketing">
+                    <input
+                        id="okayToEmail"
+                        type="checkbox"
+                        onChange={switchUp}
+                        name="okayToEmail"
+                        checked={holdValues.okayToEmail}
+                        
+                    />
+                    <label htmlFor="okayToEmail">I want to join the newsletter</label>
+                </div>
+                <button 
+                    className="form--submit"
+                >
+                    Sign up
+                </button>
+            </form>
+        </div>
+    )
 }
-
-export default App;
